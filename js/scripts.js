42facc826244e08898ed997fccec5b6ffa71c51e
Hamid Ryahi <43662608+HamidRyahi@users.noreply.github.com>
2021-03-22 07:56:44 +0100

    Add history section

--- js/scripts.js
+++ js/scripts.js
@@ -1,69 +1,136 @@
+let history = document.querySelector('.history');
+let textDisplay = document.querySelector('.text-display');
 const buttons = document.querySelectorAll('.calculator div');
-const result = document.querySelector('.text-display');
-const clear = document.querySelector('.clear');
-
-let sum = 0;
-let part1 = '0';
-let previousClick = '';
-let isSubFirst = true;
+let number1 = undefined;
+let number2 = undefined;
+let result;
+let number1FirstClick = true;
+let number2FirstClick = true;
+let currentOperation = ''
+let operator;
+let dotFirstClick = true
+let sliceFirst = true
+let isOn = false
 for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', function () {
-        if ((buttons[i].innerHTML === '=') && (previousClick === 'add')) {
-            sum += Number(part1);
-            console.log('sum', sum)
-            part1 = ''
-            result.innerHTML = sum;
+    buttons[i].addEventListener('click', function handleSubmit() {
+        if (buttons[i].id === 'clear') {
+            number1 = undefined
+            number2 = undefined
+            result = undefined
+            number1FirstClick = true
+            number2FirstClick = true
+            currentOperation = ''
+            operator = undefined
+            dotFirstClick = true
+            sliceFirst = true
+            isOn = false
+            textDisplay.innerHTML = '0'
+            history.innerHTML = '.'
+            console.log('1')
         }
-        if ((buttons[i].innerHTML === '=') && (previousClick === 'sub')) {
-            sum -= Number(part1);
-            console.log('sum', sum)
-            part1 = ''
-            result.innerHTML = sum;
-        }
-        if (buttons[i].innerHTML === 'C') {
-            sum = 0;
-            part1 = '';
-            result.innerHTML = sum;
-            isSubFirst = true
-        }
-        if (buttons[i].innerHTML === 'del') {
-            console.log('del', part1)
-            part1 = part1.slice(0, -1)
-            console.log('del', part1)
-            console.log(typeof (part1))
-            result.innerHTML = part1;
-        }
-
-        if (buttons[i].innerHTML === '+') {
-            sum += Number(part1);
-            console.log('sum', sum)
-            part1 = ''
-            result.innerHTML = sum;
-            previousClick = 'add'
-        }
-        if (buttons[i].innerHTML === '-') {
-            console.log(part1)
-            part1 = part1 * -1;
-            console.log(part1)
-            sum -= Number(part1);
-            console.log('sum', sum)
-            part1 = ''
-            result.innerHTML = sum;
-            previousClick = 'sub'
-        }
-        if (buttons[i].classList.contains('number')) {
-            if ((buttons[i].id = 'sub') && (isSubFirst === true)) {
-                console.log('sub is clicked')
-                part1 = part1.slice(0, -1)
-                part1 += buttons[i].innerHTML
-                result.innerHTML = part1;
-                isSubFirst = false;
-            } else if (buttons[i].classList.length === 1) {
-                console.log('i am a number')
-                part1 += buttons[i].innerHTML
-                console.log(part1)
-                result.innerHTML = part1;
+        if (buttons[i].id === 'equal') {
+            console.log('2')
+            if (number1 && number2) {
+                console.log('3')
+                history.innerHTML = `${number1} ${operator} ${number2} =`;
+                console.log(currentOperation)
+                result = eval(currentOperation)
+                textDisplay.innerHTML = result;
+                console.log(eval(currentOperation))
+            } else {
+                console.log('4')
+                history.innerHTML = `${result} ${buttons[i].innerHTML}`;
+                number1FirstClick = true
+                number2FirstClick = true
             }
         }
+        if (buttons[i].classList.contains('operator')) {
+            console.log('5')
+            console.log(result)
+            if (number1 && number2) {
+                console.log('6')
+                history.innerHTML = `${number1} ${buttons[i].innerHTML}`;
+                result = eval(currentOperation)
+                textDisplay.innerHTML = result;
+                number1 = result
+                number2 = 0
+                currentOperation = `${number1} ${buttons[i].innerHTML} `
+                isOn = true
+                number1FirstClick = false
+                number2FirstClick = true
+            }
+            if (result !== undefined) {
+                console.log('6')
+                history.innerHTML = `${result} ${buttons[i].innerHTML}`;
+                number1 = result
+                number2 = 0
+                currentOperation = `${number1} ${buttons[i].innerHTML} `
+                isOn = true
+                textDisplay.innerHTML = result;
+            } else {
+                console.log('7')
+                history.innerHTML = `${number1} ${buttons[i].innerHTML}`;
+                operator = buttons[i].innerHTML
+                currentOperation += buttons[i].innerHTML
+                number1FirstClick = false
+                number2FirstClick = true
+            }
+            console.log('8')
+            buttons[17].style.pointerEvents = ''
+            dotFirstClick = true
+        }
+        if (buttons[i].classList.contains('number')) {
+            if (number1FirstClick) {
+                console.log('9')
+                if (buttons[i].id === 'dot' && dotFirstClick) {
+                    textDisplay.innerHTML += buttons[i].innerHTML
+                    currentOperation += buttons[i].innerHTML
+                    number1 = Number(textDisplay.innerHTML)
+                    console.log('num1', number1)
+                    buttons[i].style.pointerEvents = 'none'
+                    dotFirstClick = false
+                } else {
+                    if (sliceFirst) {
+                        console.log('10')
+                        textDisplay.innerHTML = textDisplay.innerHTML.slice(0, -1)
+                    }
+                    console.log('10-1')
+                    textDisplay.innerHTML += buttons[i].innerHTML
+                    currentOperation += buttons[i].innerHTML
+                    number1 = Number(textDisplay.innerHTML)
+                    console.log('num1', number1)
+                    sliceFirst = false
+                }
+            } else {
+                if (buttons[i].id === 'dot' && dotFirstClick) {
+                    console.log('11')
+                    textDisplay.innerHTML += buttons[i].innerHTML
+                    currentOperation += buttons[i].innerHTML
+                    number2 = Number(textDisplay.innerHTML)
+                    console.log('num2', number2)
+                    buttons[i].style.pointerEvents = 'none'
+                    dotFirstClick = false
+                } else {
+                    if (number2FirstClick) {
+                        console.log('12')
+                        textDisplay.innerHTML = buttons[i].innerHTML
+                        currentOperation += buttons[i].innerHTML
+                        number2 = Number(textDisplay.innerHTML)
+                        console.log('num2', number2)
+                        number2FirstClick = false
+                    } else {
+                        if (isOn) {
+                            console.log('13')
+                            textDisplay.innerHTML = ''
+                            isOn = false
+                        }
+                        textDisplay.innerHTML += buttons[i].innerHTML
+                        currentOperation += buttons[i].innerHTML
+                        number2 = Number(textDisplay.innerHTML)
+                    }
+                }
+            }
+        }
+        console.log(currentOperation)
     })
 }
